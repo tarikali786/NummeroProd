20d1515f7ce3d1c9b4b24fcb79b8567caf19d67b
Tariq Ali <tarikali7444@gmail.com>
2024-12-10 17:06:32 +0530

    changes

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,9 @@
+import { Hero } from "./Hero";
+
 const Home = () => {
   return (
-    <div className="bg-blue-600 text-white-500 h-[88vh] w-full md:px-10 lg:px-12 xl:px-44 px-4 py-4">
-      <div>Home</div>
+    <div className=" text-white-500 h-[88vh]">
+      <Hero />
       <h1>Welcome To Nummero</h1>
     </div>
   );
