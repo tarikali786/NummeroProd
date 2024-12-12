d02987e6393361b323e27f698f6ab411a88cc1b8
mohammadarshid <mohammadarshid3@gmail.com>
2024-12-12 14:55:10 +0530

    changes

--- src/Component/Home/Automation.jsx
+++ src/Component/Home/Automation.jsx
@@ -1,24 +1,10 @@
-import { motion } from "framer-motion";
+import React from "react";
 
 export const Automation = () => {
   return (
     <div className=" w-full md:px-10 lg:px-12 xl:px-44 px-6 py-4 flex flex-col  lg:flex-row items-center md:items-center justify-between  gap-10">
       <div className="lg:w-1/2 ">
         <h3 className="text-2xl md:text-3xl font-bold mb-2">AI Automation</h3>
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        transition={{ duration: 0.5 }}
-        variants={{
-          hidden: { opacity: 0, x: -300 },
-          visible: { opacity: 1, x: 0 },
-        }}
-        className="lg:w-1/2"
-      >
-        <h3 className="text-2xl md:text-3xl font-bold mb-2">
-          Omni-Channel Strategy
-        </h3>
         <p className="text-[16px] mb-6  text-white-300">
           Automation in Ginee’s simplifies tasks, boosts efficiency, and
           enhances customer engagement. It automates workflows, schedules
@@ -44,7 +30,7 @@ export const Automation = () => {
           <div className="flex gap-2">
             <div className="w-20 h-12">
               <img
-                src="https://storage.googleapis.com/msgsndr/Poa647Oe1YUX8DVrwFdy/media/675a9df3b5c0944f1a5447cf.png"
+                src="https://nummero.com/wp-content/themes/nummero/newhome/media/Icons/Automation/6a.png"
                 alt="Icon"
               />
             </div>
@@ -71,24 +57,14 @@ export const Automation = () => {
             </p>
           </div>
         </div>
-      </motion.div>
+      </div>
 
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        transition={{ duration: 0.5 }}
-        variants={{
-          hidden: { opacity: 0, x: 300 },
-          visible: { opacity: 1, x: 0 },
-        }}
-        className="lg:w-1/2 w-full"
-      >
+      <div className="lg:w-1/2 w-full">
         <img
-          src="https://storage.googleapis.com/msgsndr/Poa647Oe1YUX8DVrwFdy/media/675a9f1b5f5b957ca8f07262.gif"
+          src="https://storage.googleapis.com/msgsndr/Poa647Oe1YUX8DVrwFdy/media/675993edda8c39661440bdae.gif"
           alt="Icon"
         />
-      </motion.div>
+      </div>
     </div>
   );
 };
--- src/Component/Home/Conversation.jsx
+++ src/Component/Home/Conversation.jsx
@@ -3,10 +3,6 @@ import { motion } from "framer-motion";
 export const Conversation = () => {
   return (
     <div className=" w-full md:px-10 lg:px-12 xl:px-44 px-6 py-4 flex flex-col lg:flex-row items-center md:items-center justify-between  gap-10">
-<<<<<<< HEAD
-      <div className="lg:w-1/2 ">
-        <h3 className="text-2xl md:text-3xl font-bold mb-2">AI Conversation</h3>
-=======
       <motion.div
         initial="hidden"
         whileInView="visible"
@@ -21,7 +17,6 @@ export const Conversation = () => {
         <h3 className="text-2xl md:text-3xl font-bold mb-2">
           Omni-Channel Strategy
         </h3>
->>>>>>> 191988eea2b4db3e80dad93ce7595c2b2d5a433c
         <p className="text-[16px] mb-6  text-white-300">
           "Ginee’s Conversations feature streamlines client communication and
           collaboration. It automates interactions, manages leads, and enhances
